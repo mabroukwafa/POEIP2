ad2c4c85901671197f0f8ee755b789d3443f71e1
wafa <wafaelmabrouk2000@gmail.com>
2025-04-16 14:02:57 +0200

    First Commit

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,17 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "classpath:features",
+        glue={"steps","hooks", "utils", "pages"},
+        monochrome = true,
+        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}
+        //,tags = "@POEI20252-461"
+)
+
+public class Runner {
+}
