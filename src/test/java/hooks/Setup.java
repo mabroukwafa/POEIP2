3cd604d70af761a621bc93ea476ac76bc89d9a0f
wafa <wafaelmabrouk2000@gmail.com>
2025-04-17 10:29:04 +0200

    Setup commit

--- src/test/java/hooks/Setup.java
+++ src/test/java/hooks/Setup.java
@@ -1,4 +1,69 @@
 package hooks;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import utils.ConfigReader;
+
+import java.time.Duration;
+
+
 public class Setup {
-}
+
+        public static WebDriver driver;
+
+
+        @After
+        public void takeScreenShotsOnStepFailure(Scenario scenario) {
+            if (scenario.isFailed()) {
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", scenario.getName());
+            }
+            driver.quit();
+        }
+
+        @Before
+        public void setupDriverAndPages(){
+            String url = ConfigReader.getProperty("homePageUrl");
+            int seconds = Integer.parseInt(ConfigReader.getProperty("timeout"));
+            boolean headless = Boolean.parseBoolean(ConfigReader.getProperty("headless"));
+            switch (ConfigReader.getProperty("browser").toLowerCase()){
+                case "edge":
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    if (headless) edgeOptions.addArguments("--headless");
+                    edgeOptions.addArguments("--maximize");
+                    driver = new EdgeDriver(edgeOptions);
+                    break;
+
+                case "firefox":
+                    FirefoxOptions firefoxOptions= new FirefoxOptions();
+                    if (headless) firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--maximize");
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
+
+                default:
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (headless) chromeOptions.addArguments("--headless");
+                    chromeOptions.addArguments("--maximize");
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
+            }
+
+            driver.get(url);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
+
+        }
+
+
+    }
---
+++ src/test/resources/features/2_POEI25P2G4-20_POEI25P2G4-74.feature
@@ -0,0 +1,18 @@
+@POEI25P2G4-90 @P2G4 @TNR
+@REQ_POEI25P2G4-20
+Feature: Espace client non sécurisé
+	#En saisissant un mot de passe erroné, l'utilisateur peut tout de même accéder à son espace.
+	#Test effectué avec plusieurs mots de passe différents
+
+	Background:
+		#@POEI25P2G4-74
+		Given I am on the "Sign In" page
+
+	#RG4 : En cas de connexion avec un mot de passe erroné, un message "Invalid password" s'affiche
+	@POEI25P2G4-54 @POEI25P2G4-80 @P2G4 @TConf @TNR
+	Scenario: Connexion KO
+		When I enter "valid.email@example.com" in the "Email address" field
+		And I enter "InvalidPassword01" in the "Password" field
+		And I click "Sign in"
+		Then I should see "Invalid password" error message
+		
