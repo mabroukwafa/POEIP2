e83e7490e6c2175b8af76cbcd6ba5765c24c7a67
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-17 10:11:23 +0200

    BasePage's locators

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,4 +1,32 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class BasePage {
+    @FindBy(xpath = "//img[@class='logo img-responsive']")
+    private WebElement logo;
+
+    @FindBy(xpath = "//a[@class='login']")
+    private WebElement signIn;
+
+    @FindBy(xpath = "//input[@id='search_query_top']")
+    private WebElement fieldSearch;
+
+    @FindBy(xpath = "//button[contains(@class, 'button-search')]")
+    private WebElement loupeSearch;
+
+    @FindBy(xpath = "//a[b='Cart']")
+    private WebElement accessCart;
+
+    @FindBy(xpath = "//a[@title='Women']")
+    private WebElement filtreWomen;
+
+    @FindBy(xpath = "//a[@title='Dresses']")
+    private WebElement filtreDresses;
+
+    @FindBy(xpath = "//a[@title='T-shirts']")
+    private WebElement filtreTshirts;
+
+
 }
