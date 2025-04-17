e3ee0aeb5b94c6b43c0e3effa489dc81a0d5b296
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-17 10:20:29 +0200

    BasePage constructor and functions

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
     @FindBy(xpath = "//img[@class='logo img-responsive']")
@@ -26,7 +28,44 @@ public class BasePage {
     private WebElement filtreDresses;
 
     @FindBy(xpath = "//a[@title='T-shirts']")
-    private WebElement filtreTshirts;
+    private WebElement filtreTShirts;
+
+    public BasePage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+    public void clickOnLogo(){
+        logo.click();
+    }
+
+    public void clickOnSignIn(){
+        signIn.click();
+    }
+
+    public void insertInFieldSearch(String string){
+        fieldSearch.sendKeys(string);
+    }
+
+    public void clickOnLoupeSearch(){
+        loupeSearch.click();
+    }
+
+    public void clickOnAccessCart(){
+        accessCart.click();
+    }
+
+    public void clickOnFiltreWomen(){
+        filtreWomen.click();
+    }
+
+    public void clickOnFiltreDresses(){
+        filtreDresses.click();
+    }
+
+    public void clickOnFiltreTShirts(){
+        filtreTShirts.click();
+    }
+
 
 
 }
