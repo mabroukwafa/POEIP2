11a29e084971e103c9fda027bb2fb25b71e3de04
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-18 17:03:58 +0200

    Commentaires ShoppingCartSummaryPageSteps.java

--- src/test/java/steps/ShoppingCartSummaryPageSteps.java
+++ src/test/java/steps/ShoppingCartSummaryPageSteps.java
@@ -8,10 +8,20 @@ import pages.ShoppingCartSummaryPage;
 import pages.TShirtsPage;
 
 public class ShoppingCartSummaryPageSteps {
-
+    /*
+        * Classe implémentant les étapes Cucumber relatives à la page ShoppingCartSummary.
+     */
     WebDriver driver = Setup.driver;
     ShoppingCartSummaryPage shoppingCartSummaryPage = new ShoppingCartSummaryPage(driver);
 
+    /*
+        * Implémentation de l'étape "I am redirected to the Shopping Cart Summary page".
+        * Vérifie que l'utilisateur est bien redirigé vers la page Shopping Cart Summary
+        * en confirmant l'affichage du titre spécifique à cette page.
+        *
+        * @throws AssertionError si le titre de la page Shopping Cart Summary n'est pas visible
+
+     */
     @Then("I am redirected to the <Shopping Cart Summary> page")
     public void iAmRedirectedToTheShoppingCartSummaryPage() {
         Assert.assertEquals("SHOPPING-CART SUMMARY", shoppingCartSummaryPage.getTitle());
