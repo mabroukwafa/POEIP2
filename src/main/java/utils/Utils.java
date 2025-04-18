6d053a298971c851a12ae26f0e6dc44b558a712f
Julien MICHEL <julien.michel.contact@gmail.com>
2025-04-18 08:06:17 +0200

    Added Utils class to put general methods

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,22 @@
+package utils;
+
+public class Utils {
+    public static int MAX = 26;
+    public static String generateRandomString(int length){
+        {
+            char []alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                    'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                    'o', 'p', 'q', 'r', 's', 't', 'u',
+                    'v', 'w', 'x', 'y', 'z' };
+
+            String res = "";
+            for (int i = 0; i < length; i++)
+                res += alphabet[(int) (Math.random() * 100 % MAX)];
+
+            return res;
+        }
+    }
+    public static String generateRandomEmail(int lengthBeforeAt, int lengthAfterAt){
+        return generateRandomString(lengthBeforeAt) + "@" + generateRandomString(lengthAfterAt) + ".com";
+    }
+}
