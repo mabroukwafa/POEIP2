c4cefe01eebc070713ec99dc20798a67b22b21d7
Julien MICHEL <julien.michel.contact@gmail.com>
2025-04-18 16:38:51 +0200

    Added ArticlePage

--- src/test/java/steps/SearchPageSteps.java
+++ src/test/java/steps/SearchPageSteps.java
@@ -1,4 +1,25 @@
 package steps;
 
+import hooks.Setup;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import pages.BasePage;
+import pages.SearchPage;
+
 public class SearchPageSteps {
+    WebDriver driver = Setup.driver;
+    SearchPage searchPage = new SearchPage(driver);
+
+    @Then("I am redirected to the Search page")
+    public void assertSearchPage(){
+        Assert.assertTrue(searchPage.beOnSearchPage());
+    }
+
+    @And("I see the article {string}")
+    public void iSeeTheArticle(String articleName) {
+        Assert.assertTrue(searchPage.isProductVisible(articleName));
+    }
 }
+
