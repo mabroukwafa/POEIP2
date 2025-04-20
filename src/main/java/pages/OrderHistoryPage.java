f5d1dfa7d86b613712b8aeb96cbc1cf01825df76
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-20 19:18:01 +0200

    Commentaires OrderHistoryPage

--- src/main/java/pages/OrderHistoryPage.java
+++ src/main/java/pages/OrderHistoryPage.java
@@ -4,14 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+/**
+ * Page Object Model (POM) pour la page d'historique des commandes.
+ * Elle hérite de la classe {@link BasePage}.
+ */
 public class OrderHistoryPage extends BasePage{
+
+    /**
+     * Constructeur de la classe {@link BasePage} que la classe OrderHistoryPage hérite.
+     * @param driver Le WebDriver utilisé pour naviguer et interagir avec la page
+     */
     public OrderHistoryPage(WebDriver driver){
         super(driver);
     }
 
+    //Xpath
     @FindBy(xpath = "//h1[@class='page-heading bottom-indent']\n")
     WebElement pageTitle;
 
+    /**
+     * Méthode pour récupérer le titre de la page d'historique des commandes.
+     * @return Le titre de la page d'historique des commandes
+     */
     public String pageTitle(){
     return pageTitle.getText();
     }
