2a9d958cc68c4e4b99808f620912249bdefbe5d1
Julien MICHEL <julien.michel.contact@gmail.com>
2025-04-20 13:35:03 +0200

    trying to fix Jenkinfile

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -10,7 +10,6 @@ import org.junit.runner.RunWith;
         glue={"steps","hooks", "utils", "pages"},
         monochrome = true,
         plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"}
-        ,tags = "@POEI25P2G4-71"
 )
 
 public class RunnerTest {
