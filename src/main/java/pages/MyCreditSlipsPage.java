3fe8191a8bff0c6bb4ab949c5f442e4257a31d1e
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-21 21:48:42 +0200

    Commentaires

--- src/main/java/pages/MyCreditSlipsPage.java
+++ src/main/java/pages/MyCreditSlipsPage.java
@@ -4,15 +4,28 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+/**
+ * Page Object Model (POM) pour la page des avoirs.
+ * Elle hérite de la classe {@link BasePage}.
+ */
 public class MyCreditSlipsPage extends BasePage{
 
+    /**
+     * Constructeur de la classe {@link BasePage} que la classe MyCreditSlipsPage hérite.
+     * @param webDriver Le WebDriver utilisé pour naviguer et interagir avec la page
+     */
     public MyCreditSlipsPage(WebDriver webDriver){
         super(webDriver);
     }
 
+    //XPath
     @FindBy(xpath = "//h1[@class='page-heading bottom-indent']")
     WebElement pageTitle;
 
+    /**
+     * Méthode pour récupérer le titre de la page des avoirs.
+     * @return Le texte du titre de la page
+     */
     public String pageTitle(){
         return pageTitle.getText();
     }
--- src/main/java/pages/PersonalInformationPage.java
+++ src/main/java/pages/PersonalInformationPage.java
@@ -4,15 +4,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+/**
+ * Page Object Model (POM) pour la page d'informations personnelles.
+ * Elle hérite de la classe {@link BasePage}.
+ */
 public class PersonalInformationPage extends BasePage{
+
+    /**
+     * Constructeur de la classe {@link BasePage} que la classe PersonalInformationPage hérite.
+     * @param webDriver Le WebDriver utilisé pour naviguer et interagir avec la page
+     */
     public PersonalInformationPage(WebDriver webDriver){
         super(webDriver);
     }
 
+    //XPath
     @FindBy(xpath = "//h1[@class='page-subheading']")
     WebElement pageTitle;
 
 
+    /**
+     * Méthode pour récupérer le titre de la page d'informations personnelles.
+     *
+     * @return Le texte du titre de la page
+     */
     public String getTitle(){
        return pageTitle.getText();
     }
