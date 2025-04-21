bb2ef74b5486e5e1286ed61d9ae57db27079668a
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-21 17:10:24 +0200

    Commentaires MyAddressesPageSteps,  MyCreditSlipsPageSteps, OrderHistoryPageSteps

--- src/test/java/steps/OrderHistoryPageSteps.java
+++ src/test/java/steps/OrderHistoryPageSteps.java
@@ -6,14 +6,24 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import pages.OrderHistoryPage;
 
+/**
+ * Cette classe contient les étapes de test pour la page {@link OrderHistoryPage}
+ */
 public class OrderHistoryPageSteps {
 
     WebDriver webDriver = Setup.driver;
     OrderHistoryPage orderHistoryPage = new OrderHistoryPage(webDriver);
 
+    /**
+     * Implémentation de l'étape "I see the list of my orders on the Order History page".
+     * Vérifie que l'utilisateur voit la liste de ses commandes sur la page d'historique des commandes
+     * en contrôlant l'affichage du titre spécifique à cette page.
+     *
+     * @throws AssertionError si le titre de la page d'historique des commandes n'est pas visible
+     */
     @Then("I see the list of my orders on the Order History page")
     public void iSeeTheListOfMyOrdersOnTheOrderHistoryPage(){
-        Assert.assertEquals(orderHistoryPage.pageTitle(), "ORDER HISTORY");
+        Assert.assertEquals("ORDER HISTORY", orderHistoryPage.pageTitle());
     }
 
 
