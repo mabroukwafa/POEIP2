fa0cf4d188b501b616bfa05b668e7e38a123fdfe
Asanka VARRAT <varrat.asanka@gmail.com>
2025-04-21 21:46:27 +0200

    Commentaires

--- src/test/java/steps/LoginPageSteps.java
+++ src/test/java/steps/LoginPageSteps.java
@@ -12,43 +12,79 @@ import utils.ConfigReader;
 import utils.Utils;
 
 
+/**
+ * Cette classe contient les étapes de test pour la page {@link LoginPage}
+ */
 public class LoginPageSteps {
     WebDriver webDriver = Setup.driver;
     HomePage homePage = new HomePage(webDriver);
     LoginPage loginPage = new LoginPage(webDriver);
 
 
+    /**
+     * Implémentation de l'étape "I am redirected to the Sign in page".
+     * Vérifie que l'utilisateur est redirigé vers la page de connexion
+     * en confirmant l'affichage du titre spécifique à cette page.
+     *
+     * @throws AssertionError si le titre de la page de connexion n'est pas affiché.
+     */
     @Then ("I am redirected to the Sign in page")
     public void assertOnLoginPage(){
         Assert.assertTrue(loginPage.titleIsDisplayed());
     }
 
+    /**
+     * Implémentation de l'étape "I enter {string} in the <Email address> field".
+     * @param email L'adresse e-mail à entrer dans le champ.
+     */
     @When("I enter {string} in the <Email address> field")
     public void iEnterInTheEmailAddressField(String email) {
         loginPage.sendEmail(email);
     }
 
+    /**
+     * Implémentation de l'étape "I enter {string} in the <Password> field".
+     * @param password Le mot de passe à entrer dans le champ.
+     */
     @And("I enter {string} in the <Password> field")
     public void iEnterInThePasswordField(String password) {
         loginPage.sendPassword(password);
     }
 
+    /**
+     * Implémentation de l'étape "I click <Sign in>".
+     * Cette méthode clique sur le bouton de connexion.
+     */
     @And("I click <Sign in>")
     public void iClickSignIn() {
         loginPage.clickLoginButton();
     }
 
+    /**
+     * Implémentation de l'étape "I should see {string} error message".
+     * Cette méthode vérifie qu'un message d'erreur est affiché.
+     * @param message Le message d'erreur attendu.
+     */
     @Then("I should see {string} error message")
     public void iShouldSeeErrorMessage(String message) {
         Assert.assertEquals(message, loginPage.getLoginErrorMessage());
     }
 
+    /**
+     * Implémentation de l'étape "I click on the <Forgot your password?>".
+     * Cette méthode clique sur le lien de réinitialisation du mot de passe.
+     */
     @When("I click on the <Forgot your password?>")
     public void iClickOnTheForgotYourPassword() {
         loginPage.clickForgotPassword();
     }
 
 
+    /**
+     * I enter {string} in the <Email address> field to create an account.
+     * Cette méthode remplit le champ d'adresse e-mail pour la création d'un compte.
+     * @param email L'adresse e-mail à entrer dans le champ.
+     */
     @When("I enter {string} in the <Email address> field to create an account")
     public void iEnterInTheEmailAddressFieldToCreateAnAccount(String email) {
         loginPage.sendEmailCreate(email);
@@ -69,6 +105,10 @@ public class LoginPageSteps {
     }
 
 
+    /**
+     * Implémentation de l'étape "I am logged in without a saved address".
+     * Cette méthode simule la connexion d'un utilisateur sans adresse enregistrée.
+     */
     @Given("I'm logged in without a saved address")
     public void iMLoggedInWithoutASavedAddress() {
         homePage.clickOnSignIn();
--- src/test/java/steps/MyAccountPageSteps.java
+++ src/test/java/steps/MyAccountPageSteps.java
@@ -9,42 +9,76 @@ import org.openqa.selenium.WebDriver;
 import pages.*;
 import utils.ConfigReader;
 
+/**
+ * Cette classe contient les étapes de test pour la page {@link MyAccountPage}
+ */
 public class MyAccountPageSteps {
     WebDriver webDriver = Setup.driver;
     MyAccountPage myAccountPage = new MyAccountPage(webDriver);
 
+    /**
+     * Implémentation de l'étape "I am redirected to the <My Account> page".
+     * Vérifie que l'utilisateur est bien redirigé vers la page My Account
+     * en confirmant l'affichage du titre spécifique à cette page.
+     *
+     * @throws AssertionError si le titre de la page My Account n'est pas visible
+     */
     @Then("I should be redirected to <My Account> page")
     public void iShouldBeRedirectedToMyAccountPage() {
         Assert.assertEquals("MY ACCOUNT",myAccountPage.getTitle());
     }
 
+    /**
+     * Implémentation de l'étape "I click on <My Addresses>".
+     * Effectue un clic sur le bouton "My Addresses" de la page My Account.
+     */
     @When("I click on <My Addresses>")
     public void iClickOnAddAddress() {
         myAccountPage.clickMyAddressButton();
     }
 
 
+    /**
+     * Implémentation de l'étape "I click on <Home> button".
+     * Effectue un clic sur le bouton "Home" de la page My Account.
+     */
     @When("I click on <Home> button")
     public void iClickOnHomeButton() {
         myAccountPage.clickHomeButton();
 
     }
 
+    /**
+     * Implémentation de l'étape "I click one the <Personal information> button".
+     * Effectue un clic sur le bouton "Personal information" de la page My Account.
+     */
     @When("I click on the <Personal information> button")
     public void iClickOnThePersonalInformationButton() {
         myAccountPage.clickPersonalInformationButton();
     }
 
+    /**
+     * Implémentation de l'étape "I click on the <Orders history> option".
+     * Effectue un clic sur le bouton "Orders history" de la page My Account.
+     */
     @When("I click on the <Order history> option")
     public void iClickOnTheOrderHistoryOption() {
         myAccountPage.clickOrderButton();
     }
 
+    /**
+     * Implémentation de l'étape "I click on the <My Credit Slips> option".
+     * Effectue un clic sur le bouton "My Credit Slips" de la page My Account.
+     */
     @When("I click on the <My Credit Slips> option")
     public void iClickOnTheMyCreditSlipsOption() {
         myAccountPage.clickCreditsButton();
     }
 
+    /**
+     * Implémentation de l'étape "I click on <My Addresses>".
+     * Effectue un clic sur le button d'ajout d'une nouvelle adresse de la page My Account.
+     */
     @When("I click on <Add my first Address>")
     public void iClickOnAddMyFirstAddress() {
         myAccountPage.clickAddNewAddressButton();
